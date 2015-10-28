e563ccb38b63b33395f2fc9c393defac618fc2e9
Victor <victor.kam@gmail.com>
2015-10-28 13:26:02 +0000

    finished hw

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -27,19 +27,36 @@ end
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  'Hello, ' + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !! (/[a-z&&[^aeiou]]/i.match s[0])
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if /^[01]+$/.match s
+    s.to_i(2) % 4 == 0
+  else
+    false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  def initialize(isbn_str, price_f)
+    print isbn_str
+    if isbn_str == '' or !isbn_str or price_f <= 0.0
+      raise ArgumentError
+    end
+    @isbn = isbn_str
+    @price = price_f
+  end
+  def price_as_string 
+    # like $33.98
+    "$%.2f" % @price
+  end
 end
